24097efbed3054003c19197ab6822002a784e602
Jared Fishman <jfish@gigbear.com>
2014-03-21 21:17:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CastingSlotMachine::Application.config.secret_key_base = '13b50bcbcf2c481beee61af62d5bc32730f39b3418b865b955153b310549bd32897ad1d25f6c169a03eb4e0807a69b085d474ceb615d01ce65ae2e98c56f63eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CastingSlotMachine::Application.config.session_store :cookie_store, key: '_casting_slot_machine_session'
