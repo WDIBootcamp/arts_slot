2e1e3d48072ea709ebcaae52d7d2b3f3ca4ca217
Jared Fishman <jfish@gigbear.com>
2014-03-25 11:18:58 -0700

    adjusted some design

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-jerome = User.create(name: "Jerome Allouche", career: "actor", height: 6, weight: 140, hair_color: "brown", gender: "male", email: "jerry@al.com", password: "jerome123", password_confirmation: "jerome123")
-stephanie = User.create(name: "Stephanie Daffara", career: "actor", height: 5, weight: 120, hair_color: "brown", gender: "female", email: "ste.daff@gmail.com", password: "stephanie123", password_confirmation: "stephanie123")
-herbert = User.create(name: "Herb Sneider", career: "actor", height: 6, weight: 190, hair_color: "red", gender: "male", email: "herb@google.com", password: "jerome123", password_confirmation: "jerome123")
-margaret = User.create(name: "Margaert Porch", career: "actor", height: 5, weight: 90, hair_color: "blonde", gender: "female", email: "ste.daff@gmail.com", password: "stephanie123", password_confirmation: "stephanie123")
+jerome = User.create(name: "Jerome Allouche", img: "https://dl.dropboxusercontent.com/u/9144870/225919_10101714040805306_387587273_n.jpg", career: "actor", height: 6, weight: 140, hair_color: "brown", gender: "male", email: "jerry@al.com", password: "jerome123", password_confirmation: "jerome123")
+stephanie = User.create(name: "Stephanie Daffara", img: "https://dl.dropboxusercontent.com/u/9144870/461702_10150980140229680_1451300132_o.jpg", career: "actor", height: 5, weight: 120, hair_color: "brown", gender: "female", email: "ste.daff@gmail.com", password: "stephanie123", password_confirmation: "stephanie123")
+herbert = User.create(name: "Herb Sneider", img: "https://dl.dropboxusercontent.com/u/9144870/600655_566988573395589_359691142_n.jpg", career: "actor", height: 6, weight: 190, hair_color: "red", gender: "male", email: "herb@google.com", password: "jerome123", password_confirmation: "jerome123")
